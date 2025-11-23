70a7d0df63d48e354f2fe4589f3cdca90065cc37
Craig Schardt <crschardt@fastem.com>
2025-11-22 20:00:41 -0600

    accept yaml files

--- get_image.sh
+++ get_image.sh
@@ -2,10 +2,28 @@
 set -euo pipefail
 
 url=$1
-image=$(basename ${url})
+download_path="${RUNNER_TEMP}/image"
+mkdir --parent "${download_path}"
 
-echo "Downloading ${image} from ${url}"
-wget --no-verbose ${url}
+if [[ ${url} = @("*.yaml"|"*.yml") ]]; then
+    apt-get --quiet update
+    apt-get --yes --quiet install yq
+    wget --no-verbose --output-document="manifest.yaml" "${url}"
+    echo "=== Manifest contents ==="
+    cat manifest.yaml
+    echo "========================="
+    yq -r '.urls[] | "\(.url) \(.sha256sum)"' manifest.yaml | while read -r url sha; do
+        filename="$(basename ${url})"
+        echo "Downloading: ${filename} from ${url}"
+        wget --no-verbose --output-document=${download_path}/${filename} ${url}
+        echo "$sha $filename" | sha256sum -c -
+        [[ ${filename} = *.img.xz ]] && image="${download_path}/${filename}"
+    done
+else 
+    image="${download_path}/$(basename ${url})"
+    echo "Downloading ${image} from ${url}"
+    wget --no-verbose --output-document="${image}" ${url}
+fi
 
 if [[ ${image} = *.xz ]]; then
     echo "Unzipping ${image}"
@@ -14,7 +32,7 @@ if [[ ${image} = *.xz ]]; then
 fi
 
 echo "PWD: $(pwd)"
-ls -l ${image}
+ls -l ${download_path}
 
 if [[ ${image} != *.img ]]; then
     echo "${image} isn't a valid image file"
